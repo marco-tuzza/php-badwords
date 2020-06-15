acb8fbb571132970e3b5e9711b499b31384b8180
Marco Tuzza <marcotuzza@hotmail.it>
2020-06-15 17:47:24 +0200

    Seconda versione esercizio completata

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-
 $testo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+// METODO 1
 $testo_diviso = explode(" ", $testo);
+$parola = $_GET["parola"];
+$testo_finale = str_replace($parola, "***", $testo);
+// METODO 2
 // var_dump($testo_diviso);
-$chiave = array_search('voluptate', $testo_diviso);
+// $chiave = array_search($parola, $testo_diviso);
 // var_dump($chiave);
-$sostituzione = array($chiave => "***");
-$testo_censurato = array_replace($testo_diviso, $sostituzione);
+// $sostituzione = array($chiave => "***");
+// $testo_censurato = array_replace($testo_diviso, $sostituzione);
 // var_dump($testo_censurato);
-$testo_finale = implode(" ", $testo_censurato);
+// $testo_finale = implode(" ", $testo_censurato);
 // var_dump($testo_finale);
 ?>
 
